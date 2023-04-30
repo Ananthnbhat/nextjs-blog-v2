eb4c5ce54dfe78bc0f916af0e65717e73de3dde0
Ananthnbhat <ananthnbhat2@gmail.com>
2023-04-30 11:56:41 +1000

    first commit

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,22 +1,21 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: "Ananth N Bhat's technical blog ",
+  author: 'Ananth N Bhat',
+  headerTitle: 'Blog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  // siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  // siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
-  image: '/static/images/avatar.png',
+  image: '/static/images/fancy-photo.jpg',
   socialBanner: '/static/images/twitter-card.png',
   email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
-  locale: 'en-US',
+  github: 'https://github.com/Ananthnbhat',
+  linkedin: 'https://www.linkedin.com/in/ananthnbhat',
+  facebook: 'https://www.facebook.com/profile.php?id=100010663382635',
+  // youtube: 'https://youtube.com',
+  locale: 'en-AU',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
